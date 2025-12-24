21f4ca2e61e7699db45a33071a6bcc8b4b1aa26d
ablair264 <blair@hotmail.co.uk>
2025-12-24 23:45:14 +0000

    Add stealth techniques to avoid bot detection

--- src/lib/lex-playwright/browser-manager.ts
+++ src/lib/lex-playwright/browser-manager.ts
@@ -46,13 +46,35 @@ export class LexBrowserManager {
           "--disable-setuid-sandbox",
           "--disable-dev-shm-usage",
           "--disable-gpu",
+          "--disable-blink-features=AutomationControlled",
+          "--disable-features=IsolateOrigins,site-per-process",
         ],
       });
 
       this.context = await this.browser.newContext({
         viewport: { width: 1920, height: 1080 },
         userAgent:
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+        locale: "en-GB",
+        timezoneId: "Europe/London",
+        extraHTTPHeaders: {
+          "Accept-Language": "en-GB,en;q=0.9",
+          "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+          "Accept-Encoding": "gzip, deflate, br",
+          "Connection": "keep-alive",
+          "Upgrade-Insecure-Requests": "1",
+          "Sec-Fetch-Dest": "document",
+          "Sec-Fetch-Mode": "navigate",
+          "Sec-Fetch-Site": "none",
+          "Sec-Fetch-User": "?1",
+        },
+      });
+
+      // Remove webdriver flag to avoid detection
+      await this.context.addInitScript(() => {
+        Object.defineProperty(navigator, 'webdriver', { get: () => undefined });
+        // @ts-ignore
+        delete navigator.__proto__.webdriver;
       });
 
       this.page = await this.context.newPage();
